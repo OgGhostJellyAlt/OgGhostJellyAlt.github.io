555eead142926bfc8eff172670d158497d9ee606
OgGhostJelly <johnnycambodia09@gmail.com>
2022-02-12 18:53:09 +0700

    added json support for cwjt

--- JS/loadcooking.js
+++ JS/loadcooking.js
@@ -1,73 +1,73 @@
-function load() {
-//load template
-document.getElementById("title").innerHTML = title;
-document.getElementById("disc").innerHTML = disc;
-document.getElementById("size").innerHTML = "1 Batch = " + size + " " + sizeunitb + " " + title + " " + sizeunit;
-//load favicon
-let favicon = document.createElement('link');
-favicon.setAttribute('rel','shortcut icon');
-favicon.setAttribute('href',"/IMG/" + img);
-document.querySelector('head').appendChild(favicon);
-//load img
-let image = document.createElement('img');
-image.setAttribute('src',"/IMG/" + img);
-image.setAttribute('class','one');
-document.getElementById("img").appendChild(image);
-//load recipe
-var i = 0
-do {
-let name = document.createElement('r');
-name.innerHTML = ingredients[i].name + " - "
-name.setAttribute('class','two')
-document.getElementById("recipe").appendChild(name);
+function load(recipe) {
+  //load template
+  document.getElementById("title").innerHTML = recipe.title;
+  document.getElementById("desc").innerHTML = recipe.desc;
+  document.getElementById("size").innerHTML = "1 Batch = " + recipe.size + " " + recipe.sizeunitb + " " + recipe.title + " " + recipe.sizeunit;
+  //load favicon
+  let favicon = document.createElement('link');
+  favicon.setAttribute('rel','shortcut icon');
+  favicon.setAttribute('href',"/IMG/" + recipe.img);
+  document.querySelector('head').appendChild(favicon);
+  //load img
+  let image = document.createElement('img');
+  image.setAttribute('src',"/IMG/" + recipe.img);
+  image.setAttribute('class','one');
+  document.getElementById("img").appendChild(image);
+  //load recipe
+  var i = 0
+  do {
+  let name = document.createElement('r');
+  name.innerHTML = recipe.ingredients[i].name + " - "
+  name.setAttribute('class','two')
+  document.getElementById("recipe").appendChild(name);
 
-let quantity = document.createElement('r');
-quantity.setAttribute('id', ingredients[i].name)
-quantity.innerHTML = ingredients[i].quantity
-quantity.setAttribute('class','two')
-document.getElementById("recipe").appendChild(quantity);
+  let quantity = document.createElement('r');
+  quantity.setAttribute('id', recipe.ingredients[i].name)
+  quantity.innerHTML = recipe.ingredients[i].quantity
+  quantity.setAttribute('class','two')
+  document.getElementById("recipe").appendChild(quantity);
 
-let unit = document.createElement('r');
-unit.innerHTML = ingredients[i].unit
-unit.setAttribute('class','two')
-document.getElementById("recipe").appendChild(unit);
+  let unit = document.createElement('r');
+  unit.innerHTML = recipe.ingredients[i].unit
+  unit.setAttribute('class','two')
+  document.getElementById("recipe").appendChild(unit);
 
-document.getElementById("recipe").appendChild(document.createElement('br'));
-i = i + 1
-} while (i < ingredients.length)
-//load dir
-var i = 0;
-var diri = 1;
-do {
-let direction = document.createElement("p");
-direction.setAttribute('class',dir[i].detail)
-if ( direction.getAttribute('class') == "footer" ) {
-  direction.innerHTML = dir[i].disc;
-  direction.setAttribute('class','three footer')
-} else {
-  direction.innerHTML = diri + ". " + dir[i].disc;
-  direction.setAttribute('class','three')
-  diri = diri + 1
-}
-document.getElementById('dir').appendChild(direction)
-i = i + 1;
-} while (i < dir.length);
-console.log("load finished successfully");
+  document.getElementById("recipe").appendChild(document.createElement('br'));
+  i = i + 1
+  } while (i < recipe.ingredients.length)
+  //load dir
+  var i = 0;
+  var diri = 1;
+  do {
+  let direction = document.createElement("p");
+  direction.setAttribute('class',recipe.dir[i].detail)
+  if ( direction.getAttribute('class') == "footer" ) {
+    direction.innerHTML = recipe.dir[i].desc;
+    direction.setAttribute('class','three footer')
+  } else {
+    direction.innerHTML = diri + ". " + recipe.dir[i].desc;
+    direction.setAttribute('class','three')
+    diri = diri + 1
+  }
+  document.getElementById('dir').appendChild(direction)
+  i = i + 1;
+  } while (i < recipe.dir.length);
+  console.log("load finished successfully");
 }
 
 //update batch size
-function mult() {
+function mult(recipe) {
     
-    function submit() {
+    function submit(recipe) {
       var userinput = document.getElementById('input').value;
-      document.getElementById("size").innerHTML = userinput + " Batch = " + size * userinput + " " + sizeunitb + " " + title + " " + sizeunit;
+      document.getElementById("size").innerHTML = userinput + " Batch = " + recipe.size * userinput + " " + recipe.sizeunitb + " " + recipe.title + " " + recipe.sizeunit;
       var i = 0;
 
       do {
-      document.getElementById(ingredients[i].name).innerHTML = userinput * ingredients[i].quantity;
+      document.getElementById(recipe.ingredients[i].name).innerHTML = userinput * recipe.ingredients[i].quantity;
 
       i = i + 1;
-      } while (i < ingredients.length);
+      } while (i < recipe.ingredients.length);
     }
     return {submit};
   }
