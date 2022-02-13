7c1d88b28dcb540f72821fb2b43a09f687973e97
OgGhostJelly <johnnycambodia09@gmail.com>
2022-02-13 21:15:32 +0700

    added basic blog template (gbt)

---
+++ JS/loadblog.js
@@ -0,0 +1,15 @@
+function load(blog) {
+    var i = 0
+    do {
+        var post = document.createElement('post')
+        if (!blog.post[i].link) {
+            post.innerHTML = blog.post[i].link + blog.post[i].title + "<br>" + blog.post[i].post
+        } else {
+            post.innerHTML = "<a href='" + blog.post[i].link + "'>" + blog.post[i].title + "</a>" + "<br>" + blog.post[i].post
+        }
+        document.getElementById('post').appendChild(post);
+        document.getElementById("post").appendChild(document.createElement('br'));
+
+        i = i + 1
+    } while ( i < blog.post.length )
+}
