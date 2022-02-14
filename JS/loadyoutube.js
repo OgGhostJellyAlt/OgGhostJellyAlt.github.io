f8cc61606effe01527ad4ef10ae8a4b84c5f5e31
OgGhostJelly <johnnycambodia09@gmail.com>
2022-02-14 17:51:40 +0700

    finished up youtube web

--- JS/loadyoutube.js
+++ JS/loadyoutube.js
@@ -1,11 +1,17 @@
 var videos = {}
+var channel = {}
 
-fetch("https://www.googleapis.com/youtube/v3/search?key=AIzaSyCCJ403rxQUp_0b124E49pbdsetulNTfRE&channelId=UCD1IYOrmoXBFBArQ4lSxUYQ&part=snippet,id&order=date&maxResults=20")
+fetch("https://www.googleapis.com/youtube/v3/search?key=AIzaSyCCJ403rxQUp_0b124E49pbdsetulNTfRE&channelId=UCD1IYOrmoXBFBArQ4lSxUYQ&part=snippet,id&order=date&maxResults=3")
   .then(response => response.json())
   .then(json => { videos = json ; loadvid(videos) } );
 
+fetch("https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCD1IYOrmoXBFBArQ4lSxUYQ&key=AIzaSyCCJ403rxQUp_0b124E49pbdsetulNTfRE")
+  .then(response => response.json())
+  .then(json => { channel = json ; loadchan(channel) } );
+
 //load videos
 function loadvid (videos) {
+    console.log(videos)
     var i = 0
     do {
         //div
@@ -31,6 +37,27 @@ function loadvid (videos) {
     } while ( i < videos.items.length )
 }
 
-
-
-//<iframe width="560" height="315" src="https://www.youtube.com/embed/GEXOf8zMVYA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+function loadchan (channel) {
+  //load title
+  document.getElementById('title').innerHTML = channel.items[0].snippet.title
+  //load profile
+  var stat = document.createElement('img')
+  stat.setAttribute('src',channel.items[0].snippet.thumbnails.default.url)
+  document.getElementById('stats').appendChild(stat);
+  //load subcount
+  var stat = document.createElement('h3')
+  stat.innerHTML = 'Subscribers: ' + channel.items[0].statistics.subscriberCount
+  document.getElementById('stats').appendChild(stat);
+  //load viewcount
+  var stat = document.createElement('h3')
+  stat.innerHTML = 'Total Views: ' + channel.items[0].statistics.viewCount
+  document.getElementById('stats').appendChild(stat);
+  //load totalvideos
+  var stat = document.createElement('h3')
+  stat.innerHTML = 'Total Videos: ' + channel.items[0].statistics.videoCount
+  document.getElementById('stats').appendChild(stat);
+  //load IMPORTANTDATA
+  var stat = document.createElement('h3')
+  stat.innerHTML = 'IRL Stats: NaN'
+  document.getElementById('stats').appendChild(stat);
+}
