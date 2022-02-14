bad5384400a27a3f4589cdfb0252520ccb851b52
OgGhostJelly <johnnycambodia09@gmail.com>
2022-02-14 19:09:30 +0700

    major bug fixed and visual changes

--- JS/blog/loadblog.js
+++ JS/blog/loadblog.js
@@ -2,12 +2,15 @@ function load(blog) {
     blog.post.reverse();
 
     var i = 0
+    var webi = 0
     do {
         var post = document.createElement('post')
         if (!blog.post[i].link) {
             post.innerHTML = blog.post[i].link + blog.post[i].title + "<br>" + blog.post[i].post
         } else {
-            post.innerHTML = "<a href='post?post=" + i + "'>" + blog.post[i].title + "</a>" + "<br>" + blog.post[i].post
+            post.innerHTML = "<a href='post?post=" + webi + "'>" + blog.post[i].title + "</a>" + "<br>" + blog.post[i].post
+            post.setAttribute('id',webi)
+            webi = webi + 1
         }
         document.getElementById('post').appendChild(post);
         document.getElementById("post").appendChild(document.createElement('br'));
