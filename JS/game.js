6b277d7ecbb6598df5c321d02fa1a85b7f599596
OgGhostJelly <johnnycambodia09@gmail.com>
2022-02-16 11:40:34 +0700

    update game.js

--- JS/game.js
+++ JS/game.js
@@ -1,12 +1,23 @@
+console.log('supa secret msg for gamrs')
+
 var interval = setInterval(game, 20);
 
 var player = {
-    X: '200',
-    Y: '200',
-    Xs: '25',
-    Ys: '25',
+    X: 200,
+    Y: 200,
+    Xs: 25,
+    Ys: 25,
     color: 'white',
-    score: '0'
+    score: 0
+}
+
+var bullet = {
+    X: 200,
+    Y: 200,
+    Xs: 25,
+    Ys: 25,
+    color: 'white',
+    exist: false
 }
 
 function game() {
@@ -27,12 +38,27 @@ function draw() {
     var game = document.getElementById('game').getContext('2d');
         game.fillStyle = player.color;
         game.fillRect(player.X, player.Y, player.Xs, player.Ys);
+
+        if (bullet.exist = true) {
+            var bullet = document.getElementById('game').getContext('2d');
+            game.fillStyle = bullet.color;
+            game.fillRect(bullet.X, bullet.Y, bullet.Xs, bullet.Ys);
+        }
+
+        document.getElementById('score').innerHTML = 'Score: ' + player.score
 }
 
 function keypress() {
     document.onkeyup = function (space) {
         space = space || window.event;
-            player.score = player.score + 10
-            console.log(player.score) 
+        shoot()
     };
+}
+
+function shoot() {
+    bullet.exist = true
+    bullet.X = 500
+    bullet.Y = 500
+
+    player.score = player.score + 1
 }
