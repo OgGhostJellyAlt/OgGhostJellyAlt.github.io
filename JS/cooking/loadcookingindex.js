7142d1f071094f4bda9980eefc05796ceb05df21
OgGhostJelly <johnnycambodia09@gmail.com>
2022-03-23 20:35:57 +0700

    better recipe index

--- JS/cooking/loadcookingindex.js
+++ JS/cooking/loadcookingindex.js
@@ -5,6 +5,25 @@ fetch("/JSON/cooking/recipe.json")
 function load(recipe) {
     var i = 0
     do {
+      var list = document.createElement('l')
+      list.setAttribute('class','two')
+      list.setAttribute('id',recipe.type[i].id)
+      
+      var title = document.createElement('h2')
+      title.innerHTML = recipe.type[i].display
+      list.appendChild(title)
+      list.appendChild(document.createElement('hr'))
+
+      document.getElementById('body').appendChild(list)
+      i = i + 1
+    } while ( i < recipe.type.length )
+    /*
+    <l class="two" id="dessert">
+      <h2>Dessert</h2><hr>
+    </l>
+    */
+    var i = 0
+    do {
         create = document.createElement('a');
         create.setAttribute('href','recipe?recipe=' + recipe.recipe[i].codename)
         create.setAttribute('class','link')
