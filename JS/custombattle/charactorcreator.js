e173e589514334268828b589ffbdfa02ba48ae62
OgGhostJelly <johnnycambodia09@gmail.com>
2022-05-05 20:29:17 +0700

    seperated player vars from game vars

--- JS/custombattle/charactorcreator.js
+++ JS/custombattle/charactorcreator.js
@@ -1,51 +1,58 @@
-//seperate playervar from gamevar
-var name = ''
-var level = 1
-var power = level * 12
-var stats = {
-    HP: { amount: 0, cost: 1, },
-    ATK: { amount: 0, cost: 0.2, },
-    SPD: { amount: 0, cost: 0.1 },
+var player = {
+    name: '',
+    level: 1,
+    power: 0,
+    stats: {
+        HP: { amount: 0, },
+        ATK: { amount: 0, },
+        SPD: { amount: 0, },
+    },
+    move: [
+        { name: '', DMG: 0, effect:'NONE', },
+        { name: '', DMG: 0, effect:'NONE', },
+        { name: '', DMG: 0, effect:'NONE', },
+        { name: '', DMG: 0, effect:'NONE', },
+    ],
 }
-var move = [
-    { name: '', DMG: 0, effect:'NONE', },
-    { name: '', DMG: 0, effect:'NONE', },
-    { name: '', DMG: 0, effect:'NONE', },
-    { name: '', DMG: 0, effect:'NONE', },
-]
+player.power = player.level * 12
 
+var stats = {
+    HP: { cost: 1, },
+    ATK: { cost: 0.2, },
+    SPD: { cost: 0.1, },
+}
 var effects = [
     { name: 'NONE', cost: 5, },
     { name: 'BURN', cost: 5, },
     { name: 'POISON', cost: 5, },
 ]
-document.getElementById('name').value = name
-document.getElementById('level').innerHTML = 'Level: '+level
-document.getElementById('power').innerHTML = 'POWer: '+power
-for (let i=0;i<Object.keys(stats).length;i++) {
+document.getElementById('name').value = player.name
+document.getElementById('level').innerHTML = 'Level: '+player.level
+document.getElementById('power').innerHTML = 'POWer: '+player.power
+for (let i=0;i<Object.keys(player.stats).length;i++) {
     button = document.createElement('div')
     
     symbol = document.createElement('button')
     symbol.innerHTML = '-'
-    symbol.setAttribute('onclick','stat("'+Object.keys(stats)[i]+'","-")')
+    symbol.setAttribute('onclick','stat("'+Object.keys(player.stats)[i]+'","-")')
     button.appendChild(symbol)
 
-    display = document.createElement(Object.keys(stats)[i])
-    display.innerHTML = '&nbsp'+Object.keys(stats)[i]+': '+stats[Object.keys(stats)[i]].amount+' &nbsp'
+    display = document.createElement(Object.keys(player.stats)[i])
+    display.innerHTML = '&nbsp'+Object.keys(player.stats)[i]+': '+player.stats[Object.keys(player.stats)[i]].amount+' &nbsp'
     button.appendChild(display)
 
     symbol = document.createElement('button')
     symbol.innerHTML = '+'
-    symbol.setAttribute('onclick','stat("'+Object.keys(stats)[i]+'","+")')
+    symbol.setAttribute('onclick','stat("'+Object.keys(player.stats)[i]+'","+")')
     button.appendChild(symbol)
 
-    display = document.createElement(Object.keys(stats)[i]+'cost')
-    display.innerHTML = '&nbspCosts: '+stats[Object.keys(stats)[i]].cost+' POWer'
+    display = document.createElement(Object.keys(player.stats)[i]+'cost')
+    display.innerHTML = '&nbspCosts: '+stats[Object.keys(player.stats)[i]].cost+' POWer'
     button.appendChild(display)
 
     document.getElementById('editstat').appendChild(button)
 }
-for (let i=0;i<move.length;i++) {
+for (let i=0;i<player.move.length;i++) {
     button = document.createElement('button')
     button.setAttribute('class','move')
     button.setAttribute('id','movebutton'+i)
@@ -56,8 +63,8 @@ for (let i=0;i<move.length;i++) {
             document.getElementById("move"+i).setAttribute("style","display: none;")
         }
     }.bind(i))
-    if (move[i].name) {
-        button.innerHTML = move[i].name
+    if (player.move[i].name) {
+        button.innerHTML = player.move[i].name
     } else {
         button.innerHTML = '...'
     }
@@ -73,12 +80,12 @@ for (let i=0;i<move.length;i++) {
     div.appendChild(displayname)
 
     button = document.createElement('input')
-    button.value = move[i].name
+    button.value = player.move[i].name
     button.setAttribute('id','name'+i)
     button.setAttribute('placeholder','please enter the move name...')
     button.setAttribute('maxlength','20')
     button.addEventListener('change',function(){
-        move[i].name = document.getElementById('name'+i).value
+        player.move[i].name = document.getElementById('name'+i).value
         if (document.getElementById('name'+i).value) {
             document.getElementById('movebutton'+i).innerHTML = document.getElementById('name'+i).value
         } else {
@@ -95,7 +102,7 @@ for (let i=0;i<move.length;i++) {
     div.appendChild(symbol)
 
     display = document.createElement('move'+i)
-    display.innerHTML = '&nbspDMG: '+move[i].DMG+'&nbsp'
+    display.innerHTML = '&nbspDMG: '+player.move[i].DMG+'&nbsp'
     div.appendChild(display)
 
     symbol = document.createElement('button')
@@ -117,7 +124,7 @@ for (let i=0;i<move.length;i++) {
     for(let ei=0;ei<effects.length;ei++) {
         button = document.createElement('option')
         button.innerHTML = effects[ei].name
-        if (effects[ei].name==move[i].effect) {
+        if (effects[ei].name==player.move[i].effect) {
             button.setAttribute('selected','')
         }
         select.appendChild(button)
@@ -129,31 +136,31 @@ for (let i=0;i<move.length;i++) {
 }
 function stat(me,math) {
     if(math=='-') {
-        if(stats[me].amount>0) {
-            stats[me].amount -= 1
-            power += stats[me].cost
+        if(player.stats[me].amount>0) {
+            player.stats[me].amount -= 1
+            player.power += stats[me].cost
         }
     } else {
-        if((Math.round(power*10)/10)>0) {
-            stats[me].amount += 1
-            power -= stats[me].cost
+        if((Math.round(player.power*10)/10)>0) {
+            player.stats[me].amount += 1
+            player.power -= stats[me].cost
         }
     }
-    document.getElementsByTagName(me)[0].innerHTML = '&nbsp'+me+': '+stats[me].amount+' &nbsp'
-    document.getElementById('power').innerHTML = 'POWer: '+(Math.round(power*10)/10)
+    document.getElementsByTagName(me)[0].innerHTML = '&nbsp'+me+': '+player.stats[me].amount+' &nbsp'
+    document.getElementById('power').innerHTML = 'POWer: '+(Math.round(player.power*10)/10)
 }
 function moves(me,math) {
     if(math=='-') {
-        if(move[me].DMG>0) {
-            move[me].DMG -= 1
-            power += 1
+        if(player.move[me].DMG>0) {
+            player.move[me].DMG -= 1
+            player.power += 1
         }
     } else {
-        if((Math.round(power*10)/10)>0) {
-            move[me].DMG += 1
-            power -= 1
+        if((Math.round(player.power*10)/10)>0) {
+            player.move[me].DMG += 1
+            player.power -= 1
         }
     }
-    document.getElementsByTagName('move'+me)[0].innerHTML = '&nbspDMG: '+move[me].DMG+'&nbsp'
-    document.getElementById('power').innerHTML = 'POWer: '+(Math.round(power*10)/10)
+    document.getElementsByTagName('move'+me)[0].innerHTML = '&nbspDMG: '+player.move[me].DMG+'&nbsp'
+    document.getElementById('power').innerHTML = 'POWer: '+(Math.round(player.power*10)/10)
 }
