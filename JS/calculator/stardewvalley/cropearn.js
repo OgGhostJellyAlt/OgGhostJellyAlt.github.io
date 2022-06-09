543703689764e6810533d8f31e5537b57444a967
OgGhostJelly <johnnycambodia09@gmail.com>
2022-06-09 19:13:37 +0700

    earn up

--- JS/calculator/stardewvalley/cropearn.js
+++ JS/calculator/stardewvalley/cropearn.js
@@ -1,35 +1,13 @@
 function loop() {
-    //calculate
-    var input = document.getElementById('crop').getElementsByTagName('input')  
-    var crop=[]
-    for(let i=0;i<input.length;i++) {
-        if(!!input[i].value){crop[i]={amnt:parseInt(input[i].value),$:input[i].getAttribute('$')}}else{crop[i]={amnt:0,$:input[i].getAttribute('$')}}
+    //init
+    Crops = []
+    var input = document.getElementById('crop').getElementsByTagName('input')
+    for(let i=0;i<input.length;i++){
+        let cramnt = parseFloat(input[i].value) || 0
+        let crmultiple = parseFloat(input[i].getAttribute('$')) || 0
+        Crops[i] = { amnt: cramnt, multiple: crmultiple }
     }
-
-    let sum
-    sum = 0
-    for(let i=0;i<crop.length;i++){
-        sum += crop[i].amnt
-    }
-    TotalCrops = sum
-    if (!!parseInt(document.getElementById('csc').value)){
-        Spent = TotalCrops * parseInt(document.getElementById('csc').value)}else{Spent = 0
-    }
-    
-    sum = 0
-    for(let i=0;i<crop.length;i++){
-        sum += Math.floor(crop[i].amnt * crop[i].$)
-    }
-    WorthCrops = sum
-    if (!!parseInt(document.getElementById('cbp').value)){
-        Recieved = WorthCrops * parseInt(document.getElementById('cbp').value)}else{Recieved = 0
-    }
-
-    Earning = Recieved - Spent
-    //display
-    document.getElementById('recieved').innerHTML = Recieved
-    document.getElementById('spent').innerHTML = Spent
-    document.getElementById('earnt').innerHTML = Earning
+    console.log(Crops)
     window.requestAnimationFrame(loop)
 }
 window.requestAnimationFrame(loop)
