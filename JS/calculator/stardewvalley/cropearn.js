b56ec8baf6db56eef50d1b5a6837bc03f25dc2f5
OgGhostJelly <johnnycambodia09@gmail.com>
2022-06-09 23:16:05 +0700

    updated calc

--- JS/calculator/stardewvalley/cropearn.js
+++ JS/calculator/stardewvalley/cropearn.js
@@ -1,13 +1,50 @@
+var quality = {
+    normal: 1,
+    silver: 1.25,
+    gold: 1.5,
+    iridium: 2,
+}
+
 function loop() {
-    //init
+    //INIT
+    //gets crop
     Crops = []
-    var input = document.getElementById('crop').getElementsByTagName('input')
+    let input = document.getElementById('crop').getElementsByTagName('input')
     for(let i=0;i<input.length;i++){
         let cramnt = parseFloat(input[i].value) || 0
-        let crmultiple = parseFloat(input[i].getAttribute('$')) || 0
-        Crops[i] = { amnt: cramnt, multiple: crmultiple }
+        let crquality = quality[input[i].getAttribute('quality')] || 0
+        Crops[i] = { amnt: cramnt, quality: crquality }
     }
-    console.log(Crops)
+    //gets CSC
+    var CSC = parseInt(document.getElementById('csc').value) || 0
+    //gets CBP
+    var CBP = parseInt(document.getElementById('cbp').value) || 0
+    //gets Bonus
+    var Bonus
+    //------------
+    input = document.getElementById('till')
+    if(input.checked){
+        Bonus = 1.10
+    }else{Bonus=1}
+
+    //CALC
+    //calc spent
+    var Spent = 0
+    for(let i=0;i<Crops.length;i++){
+        Spent += Crops[i].amnt * CSC
+    }
+    //calc recieved
+    var Recieved = 0
+    for(let i=0;i<Crops.length;i++){
+        Recieved += Math.floor(Crops[i].amnt * (CBP * Crops[i].quality * Bonus ))
+    }
+    //calc Earnings
+    var Earnings = Recieved - Spent
+    
+    //DISPLAY
+    document.getElementById('spent').innerHTML = Spent
+    document.getElementById('recieved').innerHTML = Recieved
+    document.getElementById('earnings').innerHTML = Earnings
     window.requestAnimationFrame(loop)
 }
 window.requestAnimationFrame(loop)
